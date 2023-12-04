89c4e1eaa0b36f882db05d662c777b909699850e
casperdijkstra <casperdijkstra92@gmail.com>
2023-12-04 12:25:46 +0100

    Refactor => Clean solution

--- Day4/Scratchcards/Program.cs
+++ Day4/Scratchcards/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text.RegularExpressions;
 using FluentAssertions;
-
 namespace Scratchcards;
 
 class Program
@@ -10,28 +9,52 @@ class Program
         var scratchCard = new ScratchCard();
         scratchCard.Solve1("dummydata").Should().Be(13);
         scratchCard.Solve1("data");
+        scratchCard.Solve2("dummydata").Should().Be(30);
+        scratchCard.Solve2("data");
     }
 }
 
 class ScratchCard
 {
     private int _totalPoints;
+    private Dictionary<int, int> _copies;
     public int Solve1(string fileName)
     {
         _totalPoints = 0;
-        foreach (var line in File.ReadAllLines($"Data/{fileName}"))
+        foreach (var card in File.ReadAllLines($"Data/{fileName}"))
         {
-            var cardNumber = Regex.Match(line, @"Card\s+\d").Value.Split()[1];
-            var myNumbers = Regex.Matches(line.Split(" | ")[0].Split(":")[1], @"\d+").Select(m => m.Value).ToList();
-            var winningNumbers = Regex.Matches(line.Split(" | ")[1], @"\d+").Select(m => m.Value).ToList();
-
-            var entriesFound = myNumbers.Count(num => winningNumbers.Contains(num));
-            int score = (int) Math.Floor(Math.Pow(2, entriesFound - 1)); 
-            // Floor because entriesFound = 0 leads to 2^-1 1/2 and should become 0. Other integers remain integers
+            var entriesFound = DetermineNumberWinningNumbers(card);
+            int score = (int) Math.Floor(Math.Pow(2, entriesFound - 1)); // Floor because entriesFound = 0 leads to 2^-1 1/2 and should become 0. Other integers remain integers
             _totalPoints += score;
         }
-
-        Console.WriteLine($"Found {_totalPoints}");
         return _totalPoints;
+    }
+
+    private static int DetermineNumberWinningNumbers(string line)
+    {
+        var myNumbers = Regex.Matches(line.Split(" | ")[0].Split(":")[1], @"\d+").Select(m => m.Value).ToList();
+        var winningNumbers = Regex.Matches(line.Split(" | ")[1], @"\d+").Select(m => m.Value).ToList();
+
+        var entriesFound = myNumbers.Count(num => winningNumbers.Contains(num));
+        return entriesFound;
+    }
+
+    public int Solve2(string fileName)
+    {
+        var allCards = File.ReadAllLines($"Data/{fileName}");
+        _copies = Enumerable.Range(0, allCards.Length).ToDictionary(x => x, x => 1);
+        _totalPoints = 0;
+        for (var index = 0; index < allCards.Length; index++)
+        {
+            var card = allCards[index];
+            var entriesFound = DetermineNumberWinningNumbers(card);
+            Enumerable.Range(0, _copies[index])
+                .SelectMany(_ => Enumerable.Range(index + 1, entriesFound))
+                .ToList()
+                .ForEach(idx => _copies[idx]++);
+        }
+
+        var totalStampCards = _copies.Sum(x => x.Value);
+        return totalStampCards;
     }
 }
