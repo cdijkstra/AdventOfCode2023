80a54032ad3b21c4aeb27a96bf90ac8770f0d8c6
casperdijkstra <casperdijkstra92@gmail.com>
2023-12-06 11:00:55 +0100

    Refactor to minimal code

--- Day06/Ferry/Program.cs
+++ Day06/Ferry/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using FluentAssertions;
-
+﻿using FluentAssertions;
 namespace Ferry;
 
 class Program
@@ -10,48 +8,63 @@ class Program
         var Ferry = new Ferry();
         Ferry.Solve1("dummydata").Should().Be(288);
         Console.WriteLine($"Answer to part 1 = {Ferry.Solve1("data")}");
-        // fertilizer.Solve2("dummydata").Should().Be(46);
-        // Console.WriteLine($"Answer to part 2 = {fertilizer.Solve2("data")}");
+        Ferry.Solve2("dummydata").Should().Be(71503);
+        Console.WriteLine($"Answer to part 2 = {Ferry.Solve2("data")}");
     }
 }
 
 class Ferry
 {
- 
+    private IEnumerable<string> _content;
     public int Solve1(string fileName)
     {
-        var content = File.ReadLines($"Data/{fileName}");
-        var times = content.ElementAt(0).Split(' ').Where(part => int.TryParse(part, out _)).Select(int.Parse).ToList();
-        var distances = content.ElementAt(1).Split(' ').Where(part => int.TryParse(part, out _)).Select(int.Parse).ToList();
+        _content = File.ReadLines($"Data/{fileName}");
+        var times = GetData(0);
+        var distances = GetData(1);
         var races = times.Zip(distances, (time, distance) => (time, distance)).ToList();
 
-        var multipliedScore = 1;
-        
-        foreach (var race in races)
-        {
-            var possibleWaysToWin = 0;
-            foreach (var buttonTime in Enumerable.Range(0, race.time - 1))
-            {
-                var velocity = buttonTime;
-                var remainingTime = race.time - buttonTime;
-                var travelledDistance = remainingTime * velocity;
-                if (travelledDistance > race.distance) possibleWaysToWin++;
-            }
-
-            multipliedScore *= possibleWaysToWin;
-        }
-        
-        return multipliedScore;
+        return races.Aggregate(1, (current, race) => MultiplyScore(race.time, race.distance, current));
     }
     
     public int Solve2(string fileName)
     {
-        var ccontent = File.ReadAllLines($"Data/{fileName}");
-        // var seedsInfo = _content
-        //     .Where(line => line.TrimStart().StartsWith(SeedsString))
-        //     .SelectMany(line => Regex.Matches(line, @"\d+").Select(match => Int64.Parse(match.Value)))
-        //     .ToList();
+        _content = File.ReadLines($"Data/{fileName}");
+        var time = GetMergedData(0);
+        var distance = GetMergedData(1);
+        
+        var multipliedScore = 1;
+        multipliedScore = MultiplyScore(time, distance, multipliedScore);
 
-        return 1;
+        return multipliedScore;
+    }
+
+    private List<int> GetData(int idx)
+    {
+        return _content.ElementAt(idx).Split(' ')
+            .Where(part => int.TryParse(part, out _))
+            .Select(int.Parse).ToList();
+    }
+
+    private long GetMergedData(int idx)
+    {
+        return long.Parse(_content.ElementAt(0)
+            .Split(' ')
+            .Where(part => long.TryParse(part, out _))
+            .Aggregate("", (acc, num) => acc + num));
+    }
+
+    private static int MultiplyScore(long time, long distance, int multipliedScore)
+    {
+        var possibleWaysToWin = 0;
+        for (var buttonTime = 0; buttonTime < time - 1; buttonTime++)
+        {
+            var velocity = buttonTime;
+            var remainingTime = time - buttonTime;
+            var travelledDistance = remainingTime * velocity;
+            if (travelledDistance > distance) possibleWaysToWin++;
+        }
+
+        multipliedScore *= possibleWaysToWin;
+        return multipliedScore;
     }
 }
