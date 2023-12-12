1c73d1a09968d71d05b5bf00b4f5ed3967612aef
casperdijkstra <casperdijkstra92@gmail.com>
2023-12-12 11:37:23 +0100

    Getting close on pt2

--- Day10/PipeMaze/Program.cs
+++ Day10/PipeMaze/Program.cs
@@ -9,6 +9,9 @@ class Program
         var maze = new Maze();
         maze.Solve1("dummydata1").Should().Be(4);
         Console.WriteLine(maze.Solve1("data"));
+        // maze.Solve2("dummydata1").Should().Be(1);
+        // maze.Solve2("dummydata3").Should().Be(8);
+        Console.WriteLine($"Answer = {maze.Solve2("data")}");
     }
 }
 
@@ -20,6 +23,8 @@ enum Direction
 class Maze
 {
     private List<List<char>> _grid = new();
+    private List<List<char>> _subGrid = new();
+
     private (int RowIndex, int ColIndex, char Value) _startIndices;
 
     private List<char> ValidFromNorth = new List<char>()
@@ -95,10 +100,161 @@ class Maze
 
         return lengths.Min();
     }
+    
+    public int Solve2(string fileName)
+    {
+        _grid = File.ReadAllLines($"Data/{fileName}")
+            .Select(line => line.ToList())
+            .ToList();
 
+        // Find entry with S
+        _startIndices = _grid.SelectMany((line, rowIndex) =>
+                line.Select((c, colIndex) => (RowIndex: rowIndex, ColIndex: colIndex, Value: c)))
+            .FirstOrDefault(cell => cell.Value == 'S');
+
+        List<(int rowIndex, int colIndex)> pipeCoordinates = new();
+        
+        // Perform algorithm for all adajacent indices
+        foreach (var neighbor in FindAdjacentIndices(_startIndices.RowIndex, _startIndices.ColIndex))
+        {
+            List<(int rowIndex, int colIndex)> possiblePipeCoordinates = new();
+            var continueLoop = true;
+            var loopLength = 1;
+            var considerEntry = neighbor;
+            while (continueLoop)
+            {
+                if (considerEntry.rowIndex < 0 || considerEntry.rowIndex >= _grid.Count ||
+                    considerEntry.colIndex < 0 || considerEntry.colIndex >= _grid[0].Count)
+                {
+                    continueLoop = false;
+                }
+                
+                var pipe = _grid[considerEntry.rowIndex][considerEntry.colIndex];
+                if (considerEntry.dir == Direction.S && !ValidFromNorth.Contains(pipe) ||
+                    considerEntry.dir == Direction.N && !ValidFromSouth.Contains(pipe) ||
+                    considerEntry.dir == Direction.E && !ValidFromWest.Contains(pipe) ||
+                    considerEntry.dir == Direction.W && !ValidFromEast.Contains(pipe))
+                {
+                    continueLoop = false;
+                }
+                
+                if (considerEntry.rowIndex == _startIndices.RowIndex && considerEntry.colIndex == _startIndices.ColIndex && loopLength > 0)
+                {
+                    possiblePipeCoordinates.Add((considerEntry.rowIndex, considerEntry.colIndex));
+                    pipeCoordinates = possiblePipeCoordinates;
+                    continueLoop = false;
+                }
+
+                if (continueLoop)
+                {
+                    possiblePipeCoordinates.Add((considerEntry.rowIndex, considerEntry.colIndex));
+                    loopLength++;
+                    considerEntry = FindNextEntry(considerEntry.rowIndex, considerEntry.colIndex, considerEntry.dir, pipe);
+                }
+            }
+        }
+
+        // Find min,max row and column in pipeCoordinates
+        var minRow = pipeCoordinates.Min(x => x.rowIndex);
+        var maxRow = pipeCoordinates.Max(x => x.rowIndex);
+        var minCol = pipeCoordinates.Min(x => x.colIndex);
+        var maxCol = pipeCoordinates.Max(x => x.colIndex);
+        // Create subgrid and apply Flooding algorithm
+
+        var padding = 1;
+        _subGrid = Enumerable.Range(0, maxRow - minRow + 1 + 2 * padding)
+            .Select(rowIndex =>
+                Enumerable.Range(0, maxCol - minCol + 1 + 2 * padding)
+                    .Select(colIndex =>
+                        (rowIndex < padding || rowIndex >= _grid.Count + padding || colIndex < padding || colIndex >= _grid[rowIndex - padding].Count + padding)
+                            ? '.'  // Padding with '.' for entries outside the original grid
+                            : _grid[rowIndex - padding][colIndex - padding]
+                    ).ToList()
+            ).ToList();
+        
+        foreach (var row in _subGrid)
+        {
+            row.ForEach(x => Console.Write(x));
+            Console.WriteLine();
+        }
+        
+        // Update possiblePipeCoordinates
+        var updatedPipeCoordinates = pipeCoordinates
+            .Select(coord => (rowIndex: coord.rowIndex + padding, colIndex: coord.colIndex + padding))
+            .ToList();
+        
+        // Now apply flooding algorithm
+        (int rowIndex, int colIndex) coordinates = (0, 0);
+        FloodCoordinateRecursively(coordinates.rowIndex, coordinates.colIndex);
+
+        var countLockedEntries = _subGrid.Sum(row => row.Count(entry => entry == '.'));
+
+        foreach (var row in _subGrid)
+        {
+            row.ForEach(x => Console.Write(x));
+            Console.WriteLine();
+        }
+        
+        return countLockedEntries;
+    }
+
+    private void FloodCoordinateRecursively(int rowIndex, int colIndex)
+    {
+        if (_subGrid[rowIndex][colIndex] != '.')
+        {
+            return;
+        }
+
+        _subGrid[rowIndex][colIndex] = '0';
+        var newCoordinates = FindAdjacentIndicesIncludingDiagonal(rowIndex, colIndex);
+        var newCoordinatesToFlood = newCoordinates.Where(indices => _subGrid[indices.rowIndex][indices.colIndex] == '.').ToList();
+        newCoordinatesToFlood.ForEach(coordinates => FloodCoordinateRecursively(coordinates.rowIndex, coordinates.colIndex));
+    }
+    
+    private List<(int rowIndex, int colIndex)> FindAdjacentIndicesIncludingDiagonal(int rowIndex, int colIndex)
+    {
+        List<(int rowIndex, int colIndex)> adjacentIndices = new();
+        if (rowIndex > 0)
+        {
+            adjacentIndices.Add((rowIndex - 1, colIndex));
+        }
+        if (rowIndex < _subGrid.Count - 1)
+        {
+            adjacentIndices.Add((rowIndex + 1, colIndex));
+        }
+        if (colIndex > 0)
+        {
+            adjacentIndices.Add((rowIndex, colIndex - 1));
+        }
+        if (colIndex < _subGrid[0].Count - 1)
+        {
+            adjacentIndices.Add((rowIndex, colIndex + 1));
+        }
+        
+        // Diagonals
+        if (rowIndex > 0 && colIndex > 0)
+        {
+            adjacentIndices.Add((rowIndex - 1, colIndex - 1));
+        }
+        if (rowIndex > 0 && colIndex < _subGrid[0].Count - 1)
+        {
+            adjacentIndices.Add((rowIndex - 1, colIndex + 1));
+        }
+        if (rowIndex < _subGrid.Count - 1 && colIndex > 0)
+        {
+            adjacentIndices.Add((rowIndex + 1, colIndex - 1));
+        }
+        if (rowIndex < _subGrid.Count - 1 && colIndex < _subGrid[0].Count - 1)
+        {
+            adjacentIndices.Add((rowIndex + 1, colIndex + 1));
+        }
+
+        return adjacentIndices;
+    }
+    
     private List<(int rowIndex, int colIndex, Direction dir)> FindAdjacentIndices(int rowIndex, int colIndex)
     {
-        List<(int, int colIndex, Direction)> adjacentIndices = new();
+        List<(int rowIndex, int colIndex, Direction)> adjacentIndices = new();
         if (rowIndex > 0)
         {
             adjacentIndices.Add((rowIndex - 1, colIndex, Direction.N));
