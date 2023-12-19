0ddf28c21eb7732a65ad1a675fd926d612fd3b3e
casperdijkstra <casperdijkstra92@gmail.com>
2023-12-20 00:03:12 +0100

    Not working yet

--- Day17/Crucible/Program.cs
+++ Day17/Crucible/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using FluentAssertions;
+﻿using FluentAssertions;
 namespace Crucible;
 
 class Program
@@ -24,6 +23,7 @@ public class Entry
     public int TotalHeatLoss { get; set; }
     public Direction Direction { get; set; }
     public int DirectionRepeat { get; set; } // Max 3
+    public List<Entry> History { get; set; } = new();
 }
 
 class Crucible
@@ -48,10 +48,10 @@ class Crucible
             ).ToList()
         ).ToList();
 
-        Entry initialEntry = _entries[1][0];
+        Entry initialEntry = _entries[0][0];
         initialEntry.TotalHeatLoss = 0;
-        Queue<Entry> queue = new();
-        queue.Enqueue(initialEntry);
+        PriorityQueue<Entry> queue = new();
+        queue.Enqueue(initialEntry, 0);
 
         List<Entry> visited = new() { initialEntry };
         List<int> totalHeatLosses = new();
@@ -69,14 +69,16 @@ class Crucible
                     var visitedEntry = visited.SingleOrDefault(visitedEntry => visitedEntry.X == neighbor.X && visitedEntry.Y == neighbor.Y);
                     if (visitedEntry != null)
                     {
-                        if (visitedEntry.TotalHeatLoss < entry.TotalHeatLoss + neighbor.HeatLoss) continue;
-                        visitedEntry.TotalHeatLoss = entry.TotalHeatLoss + neighbor.HeatLoss;
-                        queue.Enqueue(neighbor);
+                        var totalHeatLoss = entry.TotalHeatLoss + neighbor.HeatLoss;
+                        if (visitedEntry.TotalHeatLoss < totalHeatLoss) continue;
+                        visitedEntry.TotalHeatLoss = totalHeatLoss;
+                        queue.Enqueue(neighbor, totalHeatLoss);
                     }
                     else
                     {
-                        neighbor.TotalHeatLoss = entry.TotalHeatLoss + neighbor.HeatLoss;
-                        queue.Enqueue(neighbor);
+                        var totalHeatLoss = entry.TotalHeatLoss + neighbor.HeatLoss;
+                        neighbor.TotalHeatLoss = totalHeatLoss;
+                        queue.Enqueue(neighbor, totalHeatLoss);
                         visited.Add(neighbor);
                     }
                 }
@@ -89,42 +91,12 @@ class Crucible
     private List<Entry> FindValidMoves(Entry entry)
     {
         List<Entry> neighbors = new();
-        if (entry.X > 0 && entry.Direction != Direction.E)
-        {
-            // Moving west, not allowed from previous east
-            var newEntry = _entries[entry.X - 1][entry.Y];
-            newEntry.Direction = Direction.W;
-            if (entry.Direction != Direction.W)
-            {
-                newEntry.DirectionRepeat = 1;
-                neighbors.Add(newEntry);
-            }
-            else if (entry.Direction == Direction.W && entry.DirectionRepeat <= 2)
-            {
-                newEntry.DirectionRepeat = entry.DirectionRepeat + 1;
-                neighbors.Add(newEntry);
-            }
-        }
-        if (entry.X < _entries.Count - 1 && entry.Direction != Direction.W)
-        {
-            // Moving east, not allowed from previous west
-            var newEntry = _entries[entry.X + 1][entry.Y];
-            newEntry.Direction = Direction.E;
-            if (entry.Direction != Direction.E)
-            {
-                newEntry.DirectionRepeat = 1;
-                neighbors.Add(newEntry);
-            }
-            else if (entry.Direction == Direction.E && entry.DirectionRepeat <= 2)
-            {
-                newEntry.DirectionRepeat = entry.DirectionRepeat + 1;
-                neighbors.Add(newEntry);
-            }
-        }
-        if (entry.Y > 0 && entry.Direction != Direction.S)
+        if (entry.X > 0 && entry.Direction != Direction.S)
         {
             // Moving north, not allowed from previous south
-            var newEntry = _entries[entry.X][entry.Y - 1];
+            var newEntry = _entries[entry.X - 1][entry.Y];
+            newEntry.History = entry.History.Concat(new List<Entry> { newEntry }).ToList();
+            
             newEntry.Direction = Direction.N;
             if (entry.Direction != Direction.N)
             {
@@ -137,10 +109,12 @@ class Crucible
                 neighbors.Add(newEntry);
             }
         }
-        if (entry.Y < _entries[0].Count - 1 && entry.Direction != Direction.N)
+        if (entry.X < _entries.Count - 1 && entry.Direction != Direction.N)
         {
             // Moving south, not allowed from previous north
-            var newEntry = _entries[entry.X][entry.Y + 1];
+            var newEntry = _entries[entry.X + 1][entry.Y];
+            newEntry.History = entry.History.Concat(new List<Entry> { newEntry }).ToList();
+
             newEntry.Direction = Direction.S;
             if (entry.Direction != Direction.S)
             {
@@ -153,9 +127,78 @@ class Crucible
                 neighbors.Add(newEntry);
             }
         }
+        if (entry.Y > 0 && entry.Direction != Direction.E)
+        {
+            // Moving west, not allowed from previous east
+            var newEntry = _entries[entry.X][entry.Y - 1];
+            newEntry.History = entry.History.Concat(new List<Entry> { newEntry }).ToList();
+            
+            newEntry.Direction = Direction.W;                
+            if (entry.Direction != Direction.W)
+            {
+                newEntry.DirectionRepeat = 1;
+                neighbors.Add(newEntry);
+            }
+            else if (entry.Direction == Direction.W && entry.DirectionRepeat <= 2)
+            {
+                newEntry.DirectionRepeat = entry.DirectionRepeat + 1;
+                neighbors.Add(newEntry);
+            }
+        }
+        if (entry.Y < _entries[0].Count - 1 && entry.Direction != Direction.W)
+        {
+            // Moving east, not allowed from previous west
+            var newEntry = _entries[entry.X][entry.Y + 1];
+            newEntry.History = entry.History.Concat(new List<Entry> { newEntry }).ToList();
+
+            newEntry.Direction = Direction.E;
+            if (entry.Direction != Direction.E)
+            {
+                newEntry.DirectionRepeat = 1;
+                neighbors.Add(newEntry);
+            }
+            else if (entry.Direction == Direction.E && entry.DirectionRepeat <= 2)
+            {
+                newEntry.DirectionRepeat = entry.DirectionRepeat + 1;
+                neighbors.Add(newEntry);
+            }
+        }
 
         return neighbors;
     }
     
-    
+    class PriorityQueue<T>
+    {
+        private SortedDictionary<int, Queue<T>> _dictionary = new();
+
+        public int Count { get; private set; }
+
+        public void Enqueue(T item, int priority)
+        {
+            if (!_dictionary.TryGetValue(priority, out var queue))
+            {
+                queue = new Queue<T>();
+                _dictionary[priority] = queue;
+            }
+
+            queue.Enqueue(item);
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException("Queue is empty");
+
+            var firstKey = _dictionary.Keys.First();
+            var queue = _dictionary[firstKey];
+
+            var item = queue.Dequeue();
+            if (queue.Count == 0)
+                _dictionary.Remove(firstKey);
+
+            Count--;
+            return item;
+        }
+    }
 }
