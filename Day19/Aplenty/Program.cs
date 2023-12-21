dae54811f01472570833e146e44896a2ba70a136
casperdijkstra <casperdijkstra92@gmail.com>
2023-12-21 21:28:31 +0100

    Almost there pt 2

--- Day19/Aplenty/Program.cs
+++ Day19/Aplenty/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using FluentAssertions;
 
 namespace Aplenty;
@@ -10,6 +11,8 @@ class Program
         var aplenty = new Aplenty();
         aplenty.Solve1("dummydata").Should().Be(19114);
         Console.WriteLine(aplenty.Solve1("data"));
+        aplenty.Solve2("dummydata").Should().Be(167409079868000);
+
     }
 }
 
@@ -22,6 +25,8 @@ class Workflow
 class Condition
 {
     public char RequirementChar { get; set; }
+    public bool Greater { get; set; } // True for >, false for <
+    public int Value { get; set; }
     public Func<int, bool> Requirement { get; private set; }
     public string RequirementString { get; set; }
     public string Accept { get; set; }
@@ -53,12 +58,117 @@ class VariableSet
     public Dictionary<char, int> KeyValues { get; set; }
 }
 
+
+public enum Characters {
+    x,m,a,s
+}
 class Aplenty
 {
     List<Workflow> _workflows = new();
     List<VariableSet> _variableSet = new();
     
     public int Solve1(string fileName)
+    {
+        Initialize(fileName);
+
+        var totalSum = 0;
+        foreach (var variables in _variableSet)
+        {
+            var firstWorkflow = _workflows.Single(work => work.Name == "in");
+            var result = CalculateAddedValueRecursively(variables, firstWorkflow);
+            totalSum += result;
+        }
+        
+        return totalSum;
+    }
+    
+    public long Solve2(string fileName)
+    {
+        Initialize(fileName);
+
+        long totalCount = 0;
+
+        List<Dictionary<Characters, (int min, int max)>> allValues = new();
+        
+        foreach (var worflow in _workflows.Where(wf => wf.Conditions.Any(
+                     cond => cond.Reject == "A" ||
+                     cond.Accept == "A")))
+        {
+            Dictionary<Characters, (int min, int max)> dict = Enum.GetValues(typeof(Characters))
+                .Cast<Characters>()
+                .ToDictionary(
+                    character => character,
+                    range => (min: 1, max: 4000)
+                );
+
+            allValues.Add(FindContributionByReversing(dict, worflow, "A"));
+        }
+        // Remove duplicate entries... How?
+
+        return 5;
+    }
+
+    private Dictionary<Characters, (int min, int max)> FindContributionByReversing(Dictionary<Characters, (int min, int max)> dict,
+        Workflow workflow, string findWorkflow)
+    {
+        var conditions = workflow.Conditions.Where(cond => cond.Reject == findWorkflow || cond.Accept == findWorkflow).ToList();
+        foreach (var condition in conditions)
+        {
+            Characters charEnum = (Characters)Enum.Parse(typeof(Characters), condition.RequirementChar.ToString());
+            var setMinValue = dict[charEnum].min;
+            var setMaxValue = dict[charEnum].max;
+
+            if (condition.Accept == findWorkflow)
+            {
+                if (condition.Greater) // Only Greater and Smaller allowed
+                {
+                    var minValue = Math.Max(setMinValue, condition.Value + 1);
+                    var maxValue = Math.Min(setMaxValue, 4000);
+                    dict[charEnum] = (min: minValue, max: maxValue);
+                }
+                else
+                {
+                    var minValue = Math.Max(setMinValue, 1);
+                    var maxValue = Math.Min(setMaxValue, condition.Value - 1);
+                    dict[charEnum] = (min: minValue, max: maxValue);
+                }
+            }
+            if (condition.Reject == findWorkflow)
+            {
+                if (condition.Greater) // Only Greater and Smaller allowed
+                {
+                    // a > 20 -> reject when a <= 20
+                    // Smaller than or equal
+                    var minValue = Math.Max(setMinValue, 1);
+                    var maxValue = Math.Min(setMaxValue, condition.Value);
+                    dict[charEnum] = (min: minValue, max: maxValue);
+                }
+                else
+                {
+                    // a < 20 -> reject when a >= 20
+                    var minValue = Math.Max(setMinValue, condition.Value);
+                    var maxValue = Math.Min(setMaxValue, 4000);
+                    dict[charEnum] = (min: minValue, max: maxValue);
+                }
+            }
+
+        }
+        
+        if (workflow.Name == "in")
+        {
+            return dict;
+        }
+        
+        // Otherwise, call function again until we are at "in"
+        var newWorkflow = _workflows.Single(wf => wf.Conditions.Any(
+            cond => cond.Reject == workflow.Name ||
+                    cond.Accept == workflow.Name));
+
+        return FindContributionByReversing(dict, newWorkflow, workflow.Name);
+    }
+
+
+    private void Initialize(string fileName)
     {
         _workflows = new();
         _variableSet = new();
@@ -103,10 +213,16 @@ class Aplenty
                             {
                                 accept = sendToString;
                             }
+
+                            var value = requirement.Contains('>') ? 
+                                    requirement.Split(">")[1] :
+                                    requirement.Split("<")[1];
                             
                             var condition = new Condition()
                             {
                                 RequirementChar = requirement[0],
+                                Greater = requirement.Contains('>'),
+                                Value = int.Parse(value),
                                 Accept = accept,
                                 Reject = reject,
                                 RequirementString = requirement
@@ -128,21 +244,9 @@ class Aplenty
                 _variableSet.Add(new VariableSet() { KeyValues = conditions});
             }
         }
-
-        var totalSum = 0;
-        foreach (var variables in _variableSet)
-        {
-            var firstWorkflow = _workflows.Single(work => work.Name == "in");
-            var result = CalculateAddedValue(variables, firstWorkflow);
-            totalSum += result;
-        }
-        
-        bool satisfiesCondition = _workflows.First().Conditions.First().Requirement(5000);
-        
-        return totalSum;
     }
-
-    private int CalculateAddedValue(VariableSet variables, Workflow workflow)
+    
+    private int CalculateAddedValueRecursively(VariableSet variables, Workflow workflow)
     {
         foreach (var condition in workflow.Conditions) // Abort when new workflow has been found
         {
@@ -161,7 +265,7 @@ class Aplenty
                     }
 
                     var acceptWorkflow = _workflows.Single(work => work.Name == condition.Accept);
-                    return CalculateAddedValue(variables, acceptWorkflow);
+                    return CalculateAddedValueRecursively(variables, acceptWorkflow);
                 }
 
                 if (condition.Reject == String.Empty) continue;
@@ -176,7 +280,7 @@ class Aplenty
                 }
                 
                 var rejectWorkflow = _workflows.Single(work => work.Name == condition.Reject);
-                return CalculateAddedValue(variables, rejectWorkflow);
+                return CalculateAddedValueRecursively(variables, rejectWorkflow);
                 // Else try for next entry.
             }
         }
