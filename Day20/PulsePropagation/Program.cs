182e67c1d9518c23153d8335e04d00016a19ab0d
casperdijkstra <casperdijkstra92@gmail.com>
2023-12-28 23:01:10 +0100

    Relatively easily obtained answer part 1 for Day 20

---
+++ Day20/PulsePropagation/Program.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+
+namespace PulsePropagation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var pulse = new Pulse();
+        pulse.Solve1("dummydata").Should().Be(4);
+    }
+}
+
+class Flipflop
+{
+    public string Name { get; set; }
+    public bool On { get; set; }
+    public List<string> SendTo { get; set; }
+}
+
+class Conjuction
+{
+    public string Name { get; set; }
+    public bool High { get; set; }
+    public List<string> SendTo { get; set; }
+}
+
+class Pulse
+{
+    private List<Flipflop> _flipflops = new();
+    private List<Conjuction> _conjuctions = new();
+    public int Solve1(string fileName)
+    {
+        var content = File.ReadAllLines($"Data/{fileName}");
+        _flipflops = content.Where(line => line.StartsWith("%")).Select(entry =>
+        {
+            var name = entry.Split(" -> ")[0].Trim('%');
+            var sendTo = entry.Split(" -> ")[1].Split(',').Select(entry => entry.Trim()).ToList();
+            var on = false;
+            return new Flipflop
+            {
+                Name = name,
+                On = false,
+                SendTo = sendTo
+            };
+        }).ToList();
+        _conjuctions = content.Where(line => line.StartsWith("&")).Select(entry =>
+        {
+            var name = entry.Split(" -> ")[0].Trim('&');
+            var sendTo = entry.Split(" -> ")[1].Split(',').Select(entry => entry.Trim()).ToList();
+            var on = false;
+            return new Conjuction()
+            {
+                Name = name,
+                High = false,
+                SendTo = sendTo
+            };
+        }).ToList();
+
+        var broadcasterSendTo = content.Single(line => line.StartsWith("broadcaster")).Split("-> ")[1].Split(",").Select(entry => entry.Trim()).ToList(); 
+        var initialPulseHigh = false;
+        SendPulses(broadcasterSendTo, initialPulseHigh);
+        return 1;
+    }
+
+    private void SendPulses(List<string> sendTos, bool high)
+    {
+        foreach (var sendTo in sendTos)
+        {
+            if (_conjuctions.Any(x => x.Name == sendTo))
+            {
+                var conjuction = _conjuctions.Single(x => x.Name == sendTo);
+                conjuction.High = !high;
+                SendPulses(conjuction.SendTo, conjuction.High);
+            }
+            else
+            {
+                if (high) continue;
+                
+                var flipflop = _flipflops.Single(x => x.Name == sendTo);
+                flipflop.On = !flipflop.On;
+                SendPulses(flipflop.SendTo, flipflop.On);
+            }
+        }
+    }
+}
---
+++ Day21/StepCounter/Program.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+
+namespace StepCounter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var garden = new Garden();
+        garden.Solve1("dummydata", 6).Should().Be(16);
+        Console.WriteLine(garden.Solve1("data", 64));
+    }
+}
+
+class Garden
+{
+    private List<List<char>> _grid = new();
+    private List<List<char>> _newgrid = new();
+    private List<char> _notFreeGarden = new() { '#', '0' };
+    
+    public int Solve1(string fileName, int repeats)
+    {
+        _grid = File.ReadLines($"Data/{fileName}")
+            .Select(line => line.ToList())
+            .ToList();
+
+        var startCoordinates = _grid.SelectMany((row, rowIndex) =>
+            row.Select((cell, columnIndex) => new { Row = rowIndex, Column = columnIndex, Cell = cell })
+        ).FirstOrDefault(x => x.Cell == 'S');
+        
+        var coordinates = new List<(int row, int column)>() { (startCoordinates.Row, startCoordinates.Column )};
+        
+        foreach (int idx in Enumerable.Range(0, repeats))
+        {
+            List<(int row, int col)> newLocations = coordinates
+                .SelectMany(presentAt => FindPossibleLocations(presentAt.Item1, presentAt.Item2))
+                .Distinct()
+                .ToList();
+
+            coordinates = newLocations;
+        }
+        
+        coordinates.ForEach(coors =>
+        {
+            _grid[coors.row][coors.column] = '0';
+        });
+        
+        foreach (var row in _grid)
+        {
+            row.ForEach(col => Console.Write(col));
+            Console.WriteLine();
+        }
+
+        return coordinates.Count;
+    }
+
+    private List<(int row, int col)> FindPossibleLocations(int row, int col)
+    {
+        List<(int row, int col)> newCoords = new();
+        if (row > 0 && !_notFreeGarden.Contains(_grid[row - 1][col]))
+        {
+            newCoords.Add((row - 1, col));
+        }
+        if (row < _grid.Count - 1 && !_notFreeGarden.Contains(_grid[row + 1][col]))
+        {
+            newCoords.Add((row + 1, col));
+        }
+        if (col > 0 && !_notFreeGarden.Contains(_grid[row][col - 1]))
+        {
+            newCoords.Add((row, col - 1));
+        }
+        if (col < _grid[0].Count && !_notFreeGarden.Contains(_grid[row][col + 1]))
+        {
+            newCoords.Add((row, col + 1));
+        }
+
+        return newCoords;
+    }
+}
