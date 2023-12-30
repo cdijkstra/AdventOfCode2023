5ea803cc13b64295694999d11b7bb90f35e50330
casperdijkstra <casperdijkstra92@gmail.com>
2023-12-30 19:39:12 +0100

    Stuff working

--- Day22/SandSlabs/Program.cs
+++ Day22/SandSlabs/Program.cs
@@ -9,6 +9,8 @@ class Program
         var tetris = new Tetris();
         tetris.Solve1("dummydata").Should().Be(5);
         Console.WriteLine(tetris.Solve1("data"));
+        tetris.Solve2("dummydata").Should().Be(7);
+        // Console.WriteLine(tetris.Solve2("data"));
     }
 }
 
@@ -25,11 +27,155 @@ class Block
 
 class Tetris
 {
-    private Dictionary<int, List<(int x, int y, int z, int value)>> _blockCoordinateCache = new();
+    private Dictionary<int, List<(int x, int y, int z, int value)>> _blocksLocations = new();
     private List<Block> _blocks = new();
     private List<List<List<int>>> _grid = new();
+    private int _count = 0;
     public int Solve1(string fileName)
     {
+        Initialize(fileName);
+        BlocksFall();
+
+        _count = 0;
+        foreach (var block in _blocks)
+        {
+            var blockCoordinates = FindBlockCoordinates(block.entry);
+            var highestCoordinates = FindHighestBlockCoordinates(blockCoordinates);
+            
+            var blocksAbove = highestCoordinates
+                .Select(coors => _grid[coors.x][coors.y][coors.z + 1])
+                .Distinct()
+                .Where(value => value != 0)
+                .ToList();
+            
+            if (blocksAbove.All(ens => ens == 0))
+            {
+                _count++;
+            }
+            else
+            {
+                var blocksWontFall = blocksAbove.All(blockAbove =>
+                {
+                    var blockAboveCoordinates = FindBlockCoordinates(blockAbove);
+                    var lowestCoordinates = FindLowestBlockCoordinates(blockAboveCoordinates);
+
+                    return lowestCoordinates.Any(vals =>
+                        _grid[vals.x][vals.y][vals.z - 1] != 0 &&
+                        _grid[vals.x][vals.y][vals.z - 1] != block.entry);
+                });
+                
+                if (blocksWontFall)
+                {
+                    _count++;
+                }
+            }
+        }
+
+        return _count;
+    }
+    
+    public int Solve2(string fileName)
+    {
+        Initialize(fileName);
+        BlocksFall();
+
+        _count = 0;
+        foreach (var block in _blocks)
+        {
+            var blockCoordinates = FindBlockCoordinates(block.entry);
+            var highestCoordinates = FindHighestBlockCoordinates(blockCoordinates);
+            
+            var blocksAbove = highestCoordinates
+                .Select(coors => _grid[coors.x][coors.y][coors.z + 1])
+                .Distinct()
+                .Where(value => value != 0)
+                .ToList();
+            
+            if (blocksAbove.Any(ens => ens != 0))
+            {
+                var newGrid = _grid.Select(iList => iList
+                        .Select(jList => jList
+                            .Select(value => value == block.entry ? 0 : value)
+                            .ToList())
+                        .ToList())
+                    .ToList();
+                
+                ChainReaction(blocksAbove, newGrid);
+            }
+        }
+
+        return _count;
+    }
+
+    private void ChainReaction(List<int> blocksAbove, List<List<List<int>>> newGrid)
+    {
+        var blocksFalling = false;
+        List<int> newBlocksAbove = new();
+        foreach (var blockAbove in blocksAbove)
+        {
+            var blockAboveCoordinates = FindBlockCoordinates(blockAbove).ToList();
+            var lowestCoordinates = FindLowestBlockCoordinates(blockAboveCoordinates);
+            if (lowestCoordinates.All(vals => newGrid[vals.x][vals.y][vals.z - 1] == 0))
+            {
+                blocksFalling = true;
+                _count++;
+                var highestCoordinates = FindHighestBlockCoordinates(blockAboveCoordinates);
+                newBlocksAbove.AddRange(highestCoordinates
+                    .Select(coors => _grid[coors.x][coors.y][coors.z + 1])
+                    .Distinct()
+                    .Where(value => value != 0)
+                    .ToList());
+
+                newGrid = newGrid
+                    .Select(iList => iList
+                        .Select(jList => jList
+                            .Select(value => value == blockAbove ? 0 : value)
+                            .ToList())
+                        .ToList())
+                    .ToList();
+            }
+        }
+
+        if (blocksFalling && newBlocksAbove.Any())
+        {
+            var uniqueBlocksAbove = newBlocksAbove.Distinct().ToList();
+            ChainReaction(uniqueBlocksAbove, newGrid);
+        }
+    }
+
+    private void BlocksFall()
+    {
+        var continueLoop = true;
+        while (continueLoop)
+        {
+            continueLoop = false; // Unless a block fell
+            // Now let the blocks fall
+            foreach (var block in _blocks)
+            {
+                var blockCoordinates = FindBlockCoordinates(block.entry);
+                var lowestCoordinates = FindLowestBlockCoordinates(blockCoordinates);
+                var maxZ = blockCoordinates.Max(val => val.z);
+
+                if (lowestCoordinates.All(vals => vals.z == 1)) continue;
+                if (!lowestCoordinates.All(vals => _grid[vals.x][vals.y][vals.z - 1] == 0)) continue;
+
+                continueLoop = true;
+                lowestCoordinates.ForEach(coors =>
+                {
+                    _grid[coors.x][coors.y][coors.z - 1] = coors.value;
+                    _grid[coors.x][coors.y][maxZ] = 0;
+                });
+
+                var updatedCache = _blocksLocations[block.entry]
+                    .Select(data => (data.x, data.y, data.z - 1, data.value)).ToList();
+                _blocksLocations[block.entry] = updatedCache;
+            }
+        }
+    }
+
+    private void Initialize(string fileName)
+    {
+        _blocksLocations = new();
         _blocks = new();
         _grid = new();
         var entry = 0;
@@ -49,7 +195,7 @@ class Tetris
                 maxz = maxValues[2],
             });
         }
-        
+
         var maximumValueX = _blocks.Max(block => Math.Max(block.minx, block.maxx));
         var maximumValueY = _blocks.Max(block => Math.Max(block.miny, block.maxy));
         var maximumValueZ = _blocks.Max(block => Math.Max(block.minz, block.maxz));
@@ -61,9 +207,10 @@ class Tetris
                 var thirdDimension = new List<int>(Enumerable.Repeat(0, maximumValueZ + 1)); // 0 is the floor so+1
                 secondDimension.Add(thirdDimension);
             }
+
             _grid.Add(secondDimension);
         }
-        
+
         foreach (var block in _blocks)
         {
             for (int x = block.minx; x <= block.maxx; x++)
@@ -77,94 +224,6 @@ class Tetris
                 }
             }
         }
-
-        var continueLoop = true;
-        while (continueLoop)
-        {
-            continueLoop = false; // Unless a block fell
-            // Now let the blocks fall
-            foreach (var block in _blocks)
-            {
-                var blockCoordinates = FindBlockCoordinates(block.entry);
-                var lowestCoordinates = FindLowestBlockCoordinates(blockCoordinates);
-                var maxZ = blockCoordinates.Max(val => val.z);
-
-                if (lowestCoordinates.All(vals => vals.z == 1)) continue;
-                var fallingDistance = CalculateFallingDistance(lowestCoordinates.Select(item => (item.x, item.y, item.z)).ToList());
-                if (fallingDistance == 0) continue;
-                
-                continueLoop = true;
-                
-                for (int idz = 1; idz <= fallingDistance; idz++)
-                {
-                    lowestCoordinates.ForEach(coors =>
-                    {
-                        _grid[coors.x][coors.y][coors.z - idz] = coors.value;
-                        _grid[coors.x][coors.y][maxZ - idz + 1] = 0;
-                    });
-                }
-                
-                var updatedCache = _blockCoordinateCache[block.entry].Select(item => (item.x, item.y, item.z - fallingDistance, item.value)).ToList();;
-                _blockCoordinateCache[block.entry] = updatedCache;
-            }
-        }
-
-        var count = 0;
-        foreach (var block in _blocks)
-        {
-            var blockCoordinates = FindBlockCoordinates(block.entry);
-            var highestCoordinates = FindHighestBlockCoordinates(blockCoordinates);
-            
-            var blocksAbove = highestCoordinates
-                .Select(coors => _grid[coors.x][coors.y][coors.z + 1])
-                .Distinct()
-                .Where(value => value != 0)
-                .ToList();
-            
-            if (blocksAbove.All(ens => ens == 0))
-            {
-                count++;
-            }
-            else
-            {
-                var blocksWontFall = blocksAbove.All(blockAbove =>
-                {
-                    var blockAboveCoordinates = FindBlockCoordinates(blockAbove);
-                    var lowestCoordinates = FindLowestBlockCoordinates(blockAboveCoordinates);
-
-                    return lowestCoordinates.Any(vals =>
-                        _grid[vals.x][vals.y][vals.z - 1] != 0 &&
-                        _grid[vals.x][vals.y][vals.z - 1] != block.entry);
-                });
-                
-                if (blocksWontFall)
-                {
-                    count++;
-                }
-            }
-        }
-
-        return count;
-    }
-
-    private int CalculateFallingDistance(List<(int x, int y, int z)> lowestCoordinates)
-    {
-        var maxHeightToFall = lowestCoordinates[0].z - 1;
-        var fallingDistance = 0;
-        var continueFalling = true;
-        while (fallingDistance <= maxHeightToFall && continueFalling)
-        {
-            if (!lowestCoordinates.All(vals => _grid[vals.x][vals.y][vals.z - fallingDistance - 1] == 0))
-            {
-                continueFalling = false;
-            }
-            else
-            {
-                fallingDistance++;
-            }
-        }
-
-        return fallingDistance;
     }
 
     private static List<(int x, int y, int z, int value)> FindLowestBlockCoordinates(IEnumerable<(int x, int y, int z, int value)> blockCoordinates)
@@ -187,20 +246,19 @@ class Tetris
         return lowestCoordinates;
     }
 
-    private IEnumerable<(int x, int y, int z, int value)> FindBlockCoordinates(int blockEntry)
+    private List<(int x, int y, int z, int value)> FindBlockCoordinates(int blockEntry)
     {
-        if (!_blockCoordinateCache.ContainsKey(blockEntry))
+        if (_blocksLocations.TryGetValue(blockEntry, out var coordinates))
         {
-            var blockCoordinates = _grid
-                .SelectMany((xValues, x) =>
-                    xValues.SelectMany((yValues, y) =>
-                        yValues.Select((zValue, z) => (x, y, z, value: zValue))))
-                .Where(coord => coord.value == blockEntry).ToList();
-            
-            _blockCoordinateCache[blockEntry] = blockCoordinates;
-            return blockCoordinates;
+            return coordinates;
         }
         
-        return _blockCoordinateCache[blockEntry];
+        var blockCoordinates = _grid
+            .SelectMany((xValues, x) =>
+                xValues.SelectMany((yValues, y) =>
+                    yValues.Select((zValue, z) => (x, y, z, value: zValue))))
+            .Where(coord => coord.value == blockEntry).ToList();
+        _blocksLocations[blockEntry] = blockCoordinates;
+        return blockCoordinates;
     }
 }
