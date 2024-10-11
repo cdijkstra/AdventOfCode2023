9c2a77476fa892722b61485db4d5f24beae87234
Casper <c.d.dijkstra@hva.nl>
2024-10-11 15:31:07 +0200

    First setup; not every key is at LHS of dictionary...

---
+++ Day25/Snowverload/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Snowverload;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var snowVerload = new SnowverLoad();
+        snowVerload.Solve1("dummydata");
+    }
+
+    class SnowverLoad()
+    {
+        private HashSet<string> _nodes = new();
+        private Dictionary<string, List<string>> _graph = new();
+        
+        public int Solve1(string fileName)
+        {
+            foreach (var line in File.ReadLines($"Data/{fileName}"))
+            {
+                var lhs = line.Split(':')[0];
+                var rhs = line.Split(':')[1].Split().Skip(1).ToList();
+                _nodes.Add(lhs);
+                rhs.ForEach(entry => _nodes.Add(entry));
+                
+                // And add the graph
+                _graph.Add(lhs, rhs);
+            }
+
+            List<int> sizes = new();
+            
+            // Now cut wires and calculate max size graph
+            foreach (var combo in GetCombinations())
+            {
+                // Create a shallow copy of _graph
+                var newGraph = new Dictionary<string, List<string>>(_graph);
+                combo.ForEach(con => newGraph.Remove(con));
+                foreach (var key in newGraph.Keys)
+                {
+                    combo.ForEach(com => newGraph[key].RemoveAll(item => item == com));
+                }
+                
+                // Now calculate the total size of the total graph.
+                var maxSize = newGraph.Count;
+                
+                var firstEntry = newGraph.Keys.ToArray()[0];
+                Queue<string> toVisit = new();
+                toVisit.Enqueue(firstEntry);
+                List<string> visited = new();
+
+                while (toVisit.Count > 0)
+                {
+                    var node = toVisit.Dequeue();
+                    visited.Add(node);
+                    foreach (var newNode in newGraph[node].Where(n => !visited.Contains(n)))
+                    {
+                        toVisit.Enqueue(newNode);
+                    }
+                }
+
+                var size1 = visited.Count;
+                var size2 = newGraph.Count - size1;
+                var totalSize = size1 * size2;
+                if (totalSize != 0)
+                {
+                    sizes.Add(totalSize);
+                }
+            }
+
+            return sizes.Max();
+        }
+        
+        private IEnumerable<List<string>> GetCombinations()
+        {
+            for (var i = 0; i < _graph.Count; i++)
+            {
+                for (var j = i + 1; j < _graph.Count; j++)
+                {
+                    for (var k = j + 1; k < _graph.Count; k++)
+                    {
+                        var keysArray = _graph.Keys.ToArray();
+                        yield return new List<string> { keysArray[i], keysArray[j], keysArray[k] };
+                    }
+                }
+            }
+        }
+    }
+}
