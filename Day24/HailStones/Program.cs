71e77695f768b01641aae2e82bc772801723c0b3
Casper <c.d.dijkstra@hva.nl>
2024-10-17 19:21:02 +0200

    Upload calculation of formulas!

--- Day24/HailStones/Program.cs
+++ Day24/HailStones/Program.cs
@@ -1,5 +1,5 @@
 ﻿using FluentAssertions;
-using MathNet.Numerics.LinearAlgebra.Single;
+using MathNet.Numerics.LinearAlgebra.Double;
 using Expr = MathNet.Symbolics.Expression;
 
 namespace HailStones;
@@ -12,7 +12,7 @@ class Program
         simulation.Solve1("dummydata", 7, 27).Should().Be(2);
         
         simulation.Solve1("data", 200000000000000, 400000000000000);
-        // simulation.Solve2("dummydata").Should().Be(47);
+        simulation.Solve2("dummydata").Should().Be(47);
         simulation.Solve2("data"); // 684215033135104 is too high
                                           // 684195454124032 is too high
                                           // 684195261186048 is wrong
@@ -63,23 +63,16 @@ class HailSimulation
         // This means we have to add 3 hailstones to solve this system of 3 unknowns
         
         // Ax = B where x = (x,y,z,v_x,v_y,v_z)
-
-        var smallestHailstones = hailStones
-            .Select(h => new { Hailstone = h, MaxValue = h.MaxAbsValue() })
-            .OrderBy(h => h.MaxValue)
-            .Take(3)
-            .Select(h => h.Hailstone)
-            .ToList();
-
+        
         List<(long x, long y, long z)> finalPositions = new();
         List<long> finalSums = new();
-        for (int idx = 0; idx != 60; idx++)
+        for (var idx = 0; idx != hailStones.Count - 2; idx++)
         {
-            var h1 = hailStones[idx];
-            var h2 = hailStones[idx + 1];
-            var h3 = hailStones[idx + 2];
+            // Running this algorithm once should be sufficient...
+            // However the answers differ based on which hailstones are taken due to rounding errors. Take the most occurring value!
+            var h1 = hailStones[idx]; var h2 = hailStones[idx + 1]; var h3 = hailStones[idx + 2];
 
-            var A = DenseMatrix.OfArray(new float[6, 6]
+            var A = DenseMatrix.OfArray(new double[,]
             {
                 { h1.Vy - h2.Vy, h2.Vx - h1.Vx, 0, h2.Y - h1.Y, h1.X - h2.X, 0 },
                 { h1.Vy - h3.Vy, h3.Vx - h1.Vx, 0, h3.Y - h1.Y, h1.X - h3.X, 0 },
@@ -89,7 +82,7 @@ class HailSimulation
                 { h1.Vz - h3.Vz, 0, h3.Vx - h1.Vx, h3.Z - h1.Z, 0, h1.X - h3.X }
             });
 
-            var B = DenseVector.OfArray(new float[]
+            var B = DenseVector.OfArray(new double[]
             {
                 h1.X * h1.Vy - h1.Y * h1.Vx - (h2.X * h2.Vy - h2.Y * h2.Vx),
                 h1.X * h1.Vy - h1.Y * h1.Vx - (h3.X * h3.Vy - h3.Y * h3.Vx),
@@ -103,21 +96,15 @@ class HailSimulation
             var resultVector = aInverse * B;
 
             var roundedValues = resultVector.Take(3)
-                .Select(v =>
-                {
-                    var roundedValue = (long)Math.Round(v, MidpointRounding.AwayFromZero);
-                    Console.WriteLine($"Original: {v}, Rounded: {roundedValue}");
-                    return roundedValue;
-                }).ToList();
+                .Select(v => (long)Math.Round(v, MidpointRounding.AwayFromZero)).ToList();
             
-            finalPositions.Add((roundedValues[0], roundedValues[1], roundedValues[2]));
-            Console.WriteLine($"Final Result: {roundedValues.Sum()}");
             finalSums.Add(roundedValues.Sum());
         }
 
-        Console.WriteLine($"Min value = {finalSums.Min()} and max = {finalSums.Max()}; difference = {finalSums.Max() - finalSums.Min()}");
+        var mostOccurringItem = finalSums
+            .GroupBy(x => x).MaxBy(g => g.Count());
         
-        return 1;
+        return mostOccurringItem!.Key;
     }
     
     private static List<Hail> ReadInput(string fileName)
